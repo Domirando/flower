27379d8bd47d8e4211cc711edc852535ff6671ba
Domirando <maisiedev@gmail.com>
2022-02-03 14:48:47 +0500

    adding initial files

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import logo from '../assets/logo.png';
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src={logo} alt=''/>
+        </header>
+    )
+}
+export default Header
