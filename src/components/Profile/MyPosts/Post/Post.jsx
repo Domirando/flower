420e4e0a78d2a0153cd23ed302a2d76ee7e9154a
Domirando <maisiedev@gmail.com>
2022-02-08 17:29:22 +0500

    fixing router bugs

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,7 +1,7 @@
 import {ThumbDown, ThumbUp} from '@material-ui/icons';
 import styles from './Post.module.css'
 
-const Post = ({ title, description }) => {
+const Post = ({ title, description, likeCount, dislikeCount }) => {
     return (
         <div  className={styles.post}>
             <img src='https://png.pngtree.com/png-clipart/20190516/original/pngtree-cute-girl-avatar-material-png-image_4023832.jpg' alt='' />
@@ -11,11 +11,11 @@ const Post = ({ title, description }) => {
                 <div className={styles.grading_content}>
                     <span className={styles.grading}>
                         <ThumbUp className={styles.isLiked}/>
-                        <p className='grading_counter'>0</p>
+                        <p className='grading_counter'>{likeCount}</p>
                     </span>
                     <span className={styles.grading}>
                         <ThumbDown className={styles.isLiked}/>
-                        <p className='grading_counter'>0</p>
+                        <p className='grading_counter'>{dislikeCount}</p>
                     </span>
                 </div>
             </div>
