3e0d8ef72e02e3b1ecb99c0a034cd4ff700ac983
Domirando <maisiedev@gmail.com>
2022-02-08 17:51:31 +0500

    refactoring

---
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,13 @@
+import styles from "../Dialogs.module.css";
+import {NavLink} from "react-router-dom";
+import {useEffect} from 'react'
+
+const DialogItem = ({ name, id }) => {
+    let path = "/dialogs/"+id
+    return (
+        <div className={styles.dialog}>
+            <NavLink to={path} className={({isActive}) => isActive ? styles.active : undefined}>{name}</NavLink>
+        </div>
+    )
+}
+export default DialogItem
