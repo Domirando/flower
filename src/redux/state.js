2579aaf8982f4e869f2e8069a9dbe5169b78e834
Domirando <maisiedev@gmail.com>
2022-02-08 18:34:45 +0500

    refactoring

---
+++ src/redux/state.js
@@ -0,0 +1,29 @@
+let state = {
+	posts: [
+    {id: 1, title: 'Organizator?!, Web Hackathon', likesCount: 5, dislikesCount: 0},
+    {id: 2, title: 'Hello!, Global Digits!!!',  likesCount: 8, dislikesCount: 1},
+    {id: 3, title: 'Hello!, Digital Panda!!!',  likesCount: 16, dislikesCount: 0},
+    {id: 4, title: 'Hello!, Digital Panda!!!',  likesCount: 16, dislikesCount: 0},
+],
+	messages: [
+    {id: 1, message: 'hi there!'},
+    {id: 2, message: 'how are you?!'},
+    {id: 2, message: 'what are you doing?'},
+    {id: 3, message: 'what about education?'},
+    {id: 4, message: 'happy to listen!'},
+    {id: 4, message: 'Yo!'}
+],
+	dialogs: [
+    {id: 1, name: 'Tommy'},
+    {id: 2, name: 'Sara'},
+    {id: 2, name: 'Maisie'},
+    {id: 3, name: 'Sam'},
+    {id: 4, name: 'Andrew'},
+    {id: 5, name: 'Thomas'},
+    {id: 6, name: 'Simon'},
+    {id: 7, name: 'Tina'},
+    {id: 8, name: 'Simpson'}
+]
+
+
+}
