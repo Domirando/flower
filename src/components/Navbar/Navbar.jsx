088a13c2506ed4be27309cdf09a1c358ee9cbbfc
Domirando <vohidjonovnamaftuna@gmail.com>
2025-12-26 16:14:02 +0500

    feat: nav wrapper added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,30 @@
 import styles from './Navbar.module.css'
 import {NavLink} from "react-router-dom";
 import NavbarItem from "./NavbarItem";
+import { FaAngleLeft } from "react-icons/fa6";
+import {Fragment, useState} from 'react';
 
 const Navbar = () => {
     const style = {
         background: 'green',
         fontWeight: 800
     }
+    let [wrap, unWrap] = useState(false);
+
 
     return (
-        <nav className={styles.nav} >
-            <NavbarItem name='Profile' link='/'/>
-            <NavbarItem name='Messenger' link='dialogs'/>
-            <NavbarItem name='Music' link='music'/>
-            <NavbarItem name='Books' link='books'/>
-            <NavbarItem name='News' link='news'/>
+        <nav className={styles.nav}>
+            <div className={({ wrap }) => wrap ? styles.hidden : styles.flex}>
+                <NavbarItem name='Profile' link='/'/>
+                <NavbarItem name='Messenger' link='dialogs'/>
+                <NavbarItem name='Music' link='music'/>
+                <NavbarItem name='Books' link='books'/>
+                <NavbarItem name='News' link='news'/>
+            </div>
+
+            <div>
+                <FaAngleLeft onClick={() => unWrap(!wrap)}/>
+            </div>
         </nav>
     )
 }
