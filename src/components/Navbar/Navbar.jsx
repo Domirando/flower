2d0aa0e1ce3b7b5cd21539df015db3102239f857
Domirando <vohidjonovnamaftuna@gmail.com>
2025-12-28 00:16:07 +0500

    Feat: adding a toggle

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,25 @@
-import styles from './Navbar.module.css'
-import {NavLink} from "react-router-dom";
-import NavbarItem from "./NavbarItem";
-import { FaAngleLeft } from "react-icons/fa6";
-import {Fragment, useState} from 'react';
+    import styles from './Navbar.module.css'
+    import NavbarItem from "./NavbarItem";
+    import { FaAngleLeft } from "react-icons/fa6";
+    import {useState} from 'react';
 
-const Navbar = () => {
-    const style = {
-        background: 'green',
-        fontWeight: 800
+    const Navbar = () => {
+        let [wrap, unWrap] = useState(false);
+
+        return (
+            <nav className={styles.nav}>
+                <div className={ wrap ? styles.hide : styles.show}>
+                    <NavbarItem name='Profile' link='/'/>
+                    <NavbarItem name='Messenger' link='dialogs'/>
+                    <NavbarItem name='Music' link='music'/>
+                    <NavbarItem name='Books' link='books'/>
+                    <NavbarItem name='News' link='news'/>
+                </div>
+
+                <div>
+                    <FaAngleLeft onClick={() => unWrap(!wrap)}/>
+                </div>
+            </nav>
+        )
     }
-    let [wrap, unWrap] = useState('flex');
-
-    return (
-        <nav className={styles.nav}>
-            <div className={'wrap'}>
-                <NavbarItem name='Profile' link='/'/>
-                <NavbarItem name='Messenger' link='dialogs'/>
-                <NavbarItem name='Music' link='music'/>
-                <NavbarItem name='Books' link='books'/>
-                <NavbarItem name='News' link='news'/>
-            </div>
-
-            <div>
-                <FaAngleLeft onClick={() => {
-                    unWrap(wrap=='flex'?'hidden':'flex')
-                    console.log(wrap)
-                }}/>
-            </div>
-        </nav>
-    )
-}
-export default Navbar
+    export default Navbar
