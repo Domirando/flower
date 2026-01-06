96097362431bd2c2d290b2772e0899302de20573
Domirando <vohidjonovnamaftuna@gmail.com>
2026-01-06 07:40:19 +0500

    Feat: profile info changed

--- src/components/Profile/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo.jsx
@@ -5,7 +5,10 @@ const ProfileInfo = () => {
 		<div className={styles.content_main_card}>
                     <img src='https://www.collinsdictionary.com/images/full/dog_230497594.jpg'/>
                     <div className={styles.content_text}>
-                        <div>ava+description</div>
+                        <div>
+                            <b>Maftuna Vohidjonovna</b>
+                            <p>Chemical and Materials Engineer doing her research on quantum computers alongside her IT projects as she loves coding</p>
+                        </div>
                     </div>
                 </div>
 	)
