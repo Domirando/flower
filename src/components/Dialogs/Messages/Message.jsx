920191bd92c9f75689a2eea46797002c8994e037
Domirando <vohidjonovnamaftuna@gmail.com>
2026-01-08 14:23:07 +0500

    feat: restructure project into monorepo (frontend + backend)

--- src/components/Dialogs/Messages/Message.jsx
+++
@@ -1,8 +0,0 @@
-import styles from "./Messages.module.css";
-
-const Message = ({ message }) => {
-    return (
-        <span className={styles.message}>{message}</span>
-    )
-}
-export default Message
