64d9ecce2eaaf8c415bafb66f37d2d553246bcf3
Domirando <vohidjonovnamaftuna@gmail.com>
2026-01-10 07:58:28 +0500

    feat: changed

--- backend/api/auth/telegram.js
+++ backend/api/auth/telegram.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 // Vercel serverless uses default export
 export default async function handler(req, res) {
     if (req.method !== "POST") {
-        return res.status(405).json({ error: "Method not allowed" });
+        return res.status(405).json({ error: "Method not alloweddd" });
     }
 
     try {
