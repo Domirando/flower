182553dc03b8d5d6e2156a35bfa87ad2905c94c0
Domirando <vohidjonovnamaftuna@gmail.com>
2026-01-15 04:57:25 +0500

    feat: integrating with supabase

--- frontend/src/helper/supabaseClient.js
+++ frontend/src/helper/supabaseClient.js
@@ -2,7 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
-console.log("supabase key:", supabaseKey);
 
 export const supabase = createClient(
     supabaseUrl,
