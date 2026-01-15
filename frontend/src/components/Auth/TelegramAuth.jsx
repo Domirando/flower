fb6a48c2663e6ebb0e7f8294a528904b9c9115fe
Domirando <vohidjonovnamaftuna@gmail.com>
2026-01-15 08:39:38 +0500

    feat: adding an auth

--- frontend/src/components/Auth/TelegramAuth.jsx
+++ frontend/src/components/Auth/TelegramAuth.jsx
@@ -1,27 +1,25 @@
 import React, { useEffect } from "react";
 
 const TelegramAuth = ({ onAuth }) => {
+    console.log("TelegramAuth: ", TelegramAuth);
     useEffect(() => {
-        // Create global handler for Telegram auth
         window.handleTelegramAuth = function(user) {
-            console.log("Telegram user object:", user);
             alert("Logged in as: " + user.first_name);
-            if (onAuth) onAuth(user);
+            console.log("Telegram user object:", user);
         };
+
+        const script = document.createElement("script");
+        script.src = "https://telegram.org/js/telegram-widget.js?8";
+        script.async = true;
+        script.setAttribute("data-telegram-login", "DomiFlowerBot"); // no @
+        script.setAttribute("data-size", "large");
+        script.setAttribute("data-request-access", "read");
+        script.setAttribute("data-onauth", "handleTelegramAuth(user)");
+
+        document.getElementById("telegram-button").appendChild(script);
     }, [onAuth]);
 
-    return (
-        <div>
-            {/* Telegram login widget */}
-            <script
-                async
-                src="https://telegram.org/js/telegram-widget.js?15"
-                data-telegram-login="DomiFlowerBot"  // your bot username without @
-                data-size="large"
-                data-onauth="handleTelegramAuth(user)"
-            ></script>
-        </div>
-    );
+    return <div id="telegram-button"></div>;
 };
 
 export default TelegramAuth;
