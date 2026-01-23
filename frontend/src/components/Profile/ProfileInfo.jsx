1c998fe9aa585b0275b96ead7b8458779ddf1036
Domirando <vohidjonovnamaftuna@gmail.com>
2026-01-23 14:11:27 +0500

    feat: ui changed

--- frontend/src/components/Profile/ProfileInfo.jsx
+++ frontend/src/components/Profile/ProfileInfo.jsx
@@ -1,12 +1,12 @@
 import styles from './Profile.module.css'
-import state from "../../redux/state";
 
-const ProfileInfo = () => {
+const ProfileInfo = ({user}) => {
+    console.log("pf", user);
     return (
         <div className={styles.content_main_card}>
             <div className={styles.content_text}>
-                <b>{state.profilePage.user.full_name}</b>
-                <p>{state.profilePage.user.bio}</p>
+                <b>{user.full_name}</b>
+                <p>{user.bio}</p>
             </div>
             <img src='https://pbs.twimg.com/profile_images/1954804795828785152/Vmx_KtOP_400x400.jpg' alt={"avatar"}/>
         </div>
