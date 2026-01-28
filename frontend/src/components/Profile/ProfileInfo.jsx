58defbe7cd2e9d95d0983f388434d614324133f3
Domirando <vohidjonovnamaftuna@gmail.com>
2026-01-28 11:10:32 +0500

    feat: ui changed
    feat: ui changed
    
    feat: ui changed

--- frontend/src/components/Profile/ProfileInfo.jsx
+++ frontend/src/components/Profile/ProfileInfo.jsx
@@ -6,9 +6,9 @@ const ProfileInfo = ({user}) => {
         <div className={styles.content_main_card}>
             <div className={styles.content_text}>
                 <b>{user.full_name}</b>
-                <p>{user.bio}</p>
+                <p>{user.channel_id}</p>
             </div>
-            <img src='https://pbs.twimg.com/profile_images/1954804795828785152/Vmx_KtOP_400x400.jpg' alt={"avatar"}/>
+            <img src={user.ava} alt={"avatar"}/>
         </div>
     )
 }
